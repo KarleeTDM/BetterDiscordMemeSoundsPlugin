1ef5521deecad65ec09b9f5231fdf01664186618
KarleeTDM <148484053+KarleeTDM@users.noreply.github.com>
2024-08-27 13:19:27 -0400

    Update MemeSounds.plugin.js

--- MemeSounds/MemeSounds.plugin.js
+++ MemeSounds/MemeSounds.plugin.js
@@ -61,7 +61,8 @@ module.exports = (() => {
 				{ id: "vineboom", name: "Vine Boom", type: "switch", value: true },
 				{ id: "vsauce", name: "Vsauce", type: "switch", value: false },
 				{ id: "yahoo", name: "Yahoo!", type: "switch", value: true },
-				{ id: "yippee", name: "Yippee!", type: "switch", value: true }
+				{ id: "yippee", name: "Yippee!", type: "switch", value: true },
+				{ id: "💀", name: "💀", type: "switch", value: true }
 			]
 		}],
 		/* Change Log */
@@ -148,7 +149,8 @@ module.exports = (() => {
 						{re: /🗿/gmi, file: "vineboom.mp3", duration: 1115},
 						{re: /what if/gmi, file: "vsauce.mp3", duration: 7000},
 						{re: /yahoo/gmi, file: "yahoo.mp3", duration: 930},
-						{re: /yip?pee/gmi, file: "yippee.mp3", duration: 1000}
+						{re: /yip?pee/gmi, file: "yippee.mp3", duration: 1000},
+						{re: /💀/gmi, file: "💀.mp3", duration: 1000}
 					];
 
 					let lastMessageID = null;
